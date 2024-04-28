997266c623c90e830fa75327965694edec3e6485
Ayush Saklani <ayushsaklani@Ayushs-MacBook-Air.local>
2024-04-28 23:15:18 +0530

    Comments Removed From Jubit Test Case

--- src/Library_Management/Library_ManagementTest.java
+++ src/Library_Management/Library_ManagementTest.java
@@ -1,23 +1,8 @@
 package Library_Management;
 //
 import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//public class Library_ManagementTest {
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
-//
-//}
-
 import org.junit.Test;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.List;
-import java.util.Scanner;
 
 public class Library_ManagementTest {
 
@@ -96,62 +81,6 @@ public class Library_ManagementTest {
       }
   
   }
-
-//    @Test
-//    public void testDisplayBooksNonEmpty() {
-//        library.addBook("Test Book", "Test Author", 5);
-//        assertEquals("Library Catalog:\nTitle: Test Book, Author: Test Author, Available: 5\n", getOutput(() -> library.displayBooks()));
-//    }
-//
-//    @Test
-//    public void testBorrowBookSuccess() {
-//        library.addBook("Test Book", "Test Author", 1);
-//        library.borrowBook("Test Book", "User1");
-//        assertEquals("Book borrowed successfully.\n", getOutput(() -> library.borrowBook("Test Book", "User2")));
-//    }
-//
-//    @Test
-//    public void testBorrowBookFailure() {
-//        library.addBook("Test Book", "Test Author", 0);
-//        assertEquals("Sorry, this book is currently unavailable.\n", getOutput(() -> library.borrowBook("Test Book", "User1")));
-//    }
-//
-//    @Test
-//    public void testReturnBookSuccess() {
-//        library.addBook("Test Book", "Test Author", 1);
-//        library.borrowBook("Test Book", "User1");
-//        assertEquals("Book returned successfully.\n", getOutput(() -> library.returnBook("Test Book", "User1")));
-//    }
-//
-//    @Test
-//    public void testReturnBookFailure() {
-//        library.addBook("Test Book", "Test Author", 1);
-//        assertEquals("You haven't borrowed this book.\n", getOutput(() -> library.returnBook("Test Book", "User1")));
-//    }
-//
-//    @Test
-//    public void testSearchBookFound() {
-//        library.addBook("Test Book", "Test Author", 1);
-//        assertEquals("Title: Test Book, Author: Test Author, Available: 1\n", getOutput(() -> library.searchBook("Test")));
-//    }
-//
-//    @Test
-//    public void testSearchBookNotFound() {
-//        assertEquals("Book not found in the library.\n", getOutput(() -> library.searchBook("Test")));
-//    }
-//
-//    @Test
-//    public void testDisplayBorrowedBooksEmpty() {
-//        assertEquals("No books are currently borrowed.\n", getOutput(() -> library.displayBorrowedBooks()));
-//    }
-//
-//    @Test
-//    public void testDisplayBorrowedBooksNonEmpty() {
-//        library.addBook("Test Book", "Test Author", 1);
-//        library.borrowBook("Test Book", "User1");
-//        assertEquals("Borrowed Books:\nTitle: Test Book, Author: Test Author, Borrower: User1, Copies Borrowed: 1\n", getOutput(() -> library.displayBorrowedBooks()));
-//    }
-//
   
 }
 
